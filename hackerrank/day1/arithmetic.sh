ee3302c64fc73b96042e8ed26c2f770fb62ee6e2
Shyam <36335647+shyamnayak6@users.noreply.github.com>
2020-06-28 12:59:39 +0530

    Update arithmetic.sh

--- hackerrank/day1/arithmetic.sh
+++ hackerrank/day1/arithmetic.sh
@@ -2,6 +2,6 @@
 
 #We provide you with expressions containing +,-,*,^, / and parenthesis. None of the numbers in the expression involved will exceed 999 .
 #Your task is to evaluate the expression and display the correct output rounding upto  decimal places.
-#read sum
 
-echo "scale=3; $sum" | bc
+read A
+printf "%.3f" $(echo "$A" | bc -l)
